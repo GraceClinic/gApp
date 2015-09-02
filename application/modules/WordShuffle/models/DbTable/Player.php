21e293f53d0f460aea81fa56a402068c094f957b
Santosh-Ratnala <santoshratnala91@gmail.com>
2015-09-02 15:10:40 +0530

    Player database table class.

---
+++ application/modules/WordShuffle/models/DbTable/Player.php
@@ -0,0 +1,13 @@
+<?php
+class WordShuffle_Model_DbTable_Player extends Common_Abstracts_DbTable
+{
+    protected $_primary = 'id';
+    protected $_name = 'Player';
+
+
+}
+
+/*
+ * when you set primary key as an array, then it doesnt give any problem
+ *
+ * */
