5c056942c778582897ce17a00b2036be9d1796b0
Ämit Chigadani <amitchigadani@gmail.com>
2016-07-13 17:00:38 +0530

    Created two new physical entities (tables) notebook and notes. Deleted 4 table; notes, notes_wsround, notes_player, notes_wsgame.

--- dev/design output/MySQL/schema/20160713-1_update.sql
+++ dev/design output/MySQL/schema/20160713-1_update.sql
@@ -1,54 +1,53 @@
-#All Purpose Notebook ------ 1
 
 USE gapp;
 DROP TABLE IF EXISTS notes ;
-USE gapp;
+DROP TABLE IF EXISTS notebook ;
+
+CREATE TABLE `gapp`.`notebook` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NULL DEFAULT 'My NoteBook',
+  PRIMARY KEY (`id`));
+
+ALTER TABLE `gapp`.`player`
+ADD COLUMN `idNoteBook` INT UNSIGNED NULL AFTER `modifyDate`,
+ADD UNIQUE INDEX `idNoteBook_UNIQUE` (`idNoteBook` ASC);
+ALTER TABLE `gapp`.`player`
+ADD CONSTRAINT `fkPlayer2NoteBook`
+  FOREIGN KEY (`idNoteBook`)
+  REFERENCES `gapp`.`notebook` (`id`)
+  ON DELETE SET NULL
+  ON UPDATE CASCADE;
+
+ALTER TABLE `gapp`.`wsgame`
+ADD COLUMN `idNoteBook` INT UNSIGNED NULL AFTER `status`,
+ADD UNIQUE INDEX `idNoteBook_UNIQUE` (`idNoteBook` ASC);
+ALTER TABLE `gapp`.`wsgame`
+ADD CONSTRAINT `fkGame2NoteBook`
+  FOREIGN KEY (`idNoteBook`)
+  REFERENCES `gapp`.`notebook` (`id`)
+  ON DELETE SET NULL
+  ON UPDATE CASCADE;
+
+  ALTER TABLE `gapp`.`wsround`
+ADD COLUMN `idNoteBook` INT UNSIGNED NULL AFTER `index`,
+ADD UNIQUE INDEX `idNoteBook_UNIQUE` (`idNoteBook` ASC);
+ALTER TABLE `gapp`.`wsround`
+ADD CONSTRAINT `fkRound2NoteBook`
+  FOREIGN KEY (`idNoteBook`)
+  REFERENCES `gapp`.`notebook` (`id`)
+  ON DELETE SET NULL
+  ON UPDATE CASCADE;
+
 CREATE TABLE `notes` (
-  `idNotes` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `title` varchar(45) DEFAULT NULL,
   `category` enum('funny','todo','remember','remarkable') NOT NULL DEFAULT 'funny',
   `body` varchar(500) DEFAULT NULL,
-  PRIMARY KEY (`idNotes`)
+  `idNoteBook` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fkNotes2NoteBook_idx` (`idNoteBook`),
+  CONSTRAINT `fkNotes2NoteBook` FOREIGN KEY (`idNoteBook`) REFERENCES `notebook` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
-
-USE gapp;
-DROP TABLE IF EXISTS notes_player ;
-USE gapp;
-CREATE TABLE `notes_player` (
-  `idnotes` int(10) unsigned NOT NULL,
-  `idplayer` int(10) unsigned NOT NULL,
-  PRIMARY KEY (`idnotes`,`idplayer`),
-  UNIQUE KEY `idnotes_UNIQUE` (`idnotes`),
-  KEY `fk_idplayer_idx` (`idplayer`),
-  CONSTRAINT `fk_idnotes_notesplayer` FOREIGN KEY (`idnotes`) REFERENCES `notes` (`idNotes`) ON DELETE CASCADE ON UPDATE NO ACTION,
-  CONSTRAINT `fk_idplayer` FOREIGN KEY (`idplayer`) REFERENCES `player` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-USE gapp;
-DROP TABLE IF EXISTS notes_wsgame ;
-USE gapp;
-CREATE TABLE `notes_wsgame` (
-  `idnotes` int(10) unsigned NOT NULL,
-  `idWSgame` int(10) unsigned NOT NULL,
-  PRIMARY KEY (`idnotes`,`idWSgame`),
-  UNIQUE KEY `idnotes_UNIQUE` (`idnotes`),
-  KEY `fk_idWSGame_idx` (`idWSgame`),
-  CONSTRAINT `fk_idWSGame` FOREIGN KEY (`idWSgame`) REFERENCES `wsgame` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
-  CONSTRAINT `fk_idnotes_noteswsgame` FOREIGN KEY (`idnotes`) REFERENCES `notes` (`idNotes`) ON DELETE CASCADE ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-USE gapp;
-DROP TABLE IF EXISTS notes_wsround ;
-USE gapp;
-CREATE TABLE `notes_wsround` (
-  `idnotes` int(10) unsigned NOT NULL,
-  `idround` int(11) NOT NULL,
-  PRIMARY KEY (`idnotes`,`idround`),
-  UNIQUE KEY `idnotes_UNIQUE` (`idnotes`),
-  KEY `fk_idwsround_idx` (`idround`),
-  CONSTRAINT `fk_idnotes_noteswsround` FOREIGN KEY (`idnotes`) REFERENCES `notes` (`idNotes`) ON DELETE CASCADE ON UPDATE NO ACTION,
-  CONSTRAINT `fk_idwsround` FOREIGN KEY (`idround`) REFERENCES `wsround` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 #PopulateGames() ---- 2
 
@@ -78,26 +77,26 @@ DECLARE idpageCheck INT;
  SET idInstruction=101;
  SET idPage=101;
  SET nameTitleNo=1;
- 
+
 SELECT id into idgameCheck FROM gapp.games WHERE id >= 101 order by id LIMIT 1;
 SELECT id into idinstructionCheck FROM gapp.instructions WHERE id >= 101 order by id LIMIT 1;
 SELECT id into idpageCheck FROM gapp.page WHERE id >= 101 order by id LIMIT 1;
- 
+
 if(idgameCheck is null and idinstructionCheck is null and idpageCheck is null)
  then
  WHILE i  <= noOfGames DO
   SET j = 1;
-  
+
  insert into gapp.games values (idGame,CONCAT('Name_',nameTitleNo));
  SET nameTitleNo = nameTitleNo + 1;
- 
+
  SET instnCnt = (SELECT COUNT(*) FROM gapp.instructions);
  SET modRes = instnCnt mod 9;
  SET modRes = modRes+ 1;
  insert into gapp.instructions values (idInstruction, idGame, CONCAT('Title_',nameTitleNo), CONCAT('app/assets/wordshuffle/testing/test-',modRes,'.png'));
- 
+
  SET instnRecordsCnt = (SELECT COUNT(*) FROM gapp.instructions);
- 
+
 	IF ((instnRecordsCnt mod 3 = 0) AND (instnRecordsCnt mod 5 = 0))
 	 then
 		SET modCnt=4;
@@ -109,21 +108,21 @@ if(idgameCheck is null and idinstructionCheck is null and idpageCheck is null)
 		SET modCnt=5;
 	else
 		SET modCnt=2;
-		
+
 	END IF;
- 
+
  WHILE j  <= modCnt DO
  insert into gapp.page values (idPage,idInstruction,CONCAT('app/assets/wordshuffle/testing/test-',j,'.html'),j);
  SET idPage = idPage + 1;
  SET j = j + 1;
  END WHILE;
- 
- SET  i = i + 1; 
+
+ SET  i = i + 1;
  SET  idInstruction = idInstruction + 1;
  SET  idGame = idGame + 1;
- 
+
 END WHILE;
-else 
+else
 select 'Primary Key value greater than 101 already exist in one of the tables' as Warning;
 end if;
 END$$
